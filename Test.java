8c3b3ac421fb6cd5ebe4c89dfab27ca2dd227e37
Sathwik H S <89410515+Sat1357@users.noreply.github.com>
2022-08-16 12:14:30 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,14 @@
+package assignment;
+
+public class Test {
+
+	public static void main(String[] args) {
+		HPNoteBook h = new HPNoteBook();
+		h.click();
+		h.scroll();
+		DellNoteBook d = new DellNoteBook();
+		d.click();
+		d.scroll();
+	}
+
+}
